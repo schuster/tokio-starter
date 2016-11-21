728886dc09bed2272dad9d94f01300ebab9f95f7
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-11-20 21:34:47 -0500

    Add start of a scatter/gather approach

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,51 @@ extern crate tokio_service;
 use tokio_core::reactor::Core;
 use tokio_service::Service;
 
-struct SearchService {}
+struct SearchService {
+    searcher_a: SegmentSearchService,
+    searcher_b: SegmentSearchService,
+    searcher_c: SegmentSearchService
+}
 
 impl SearchService {
     fn new() -> SearchService {
-        SearchService {}
+        SearchService {
+            // TODO: initialize each with a different dictionary
+            searcher_a: SegmentSearchService::new(),
+            searcher_b: SegmentSearchService::new(),
+            searcher_c: SegmentSearchService::new(),
+        }
     }
 }
 
+// TODO: change this to return a *set* of results
 impl Service for SearchService {
+    type Request = String;
+    type Response = Vec<String>;
+    type Error = std::io::Error;
+    type Future = futures::Collect<
+
+            Vec<futures::Done<String, Self::Error>>>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        // TODO: maybe return a Maybe String instead
+        futures::collect(
+            vec![self.searcher_a.call(req.clone()),
+                 self.searcher_b.call(req.clone()),
+                 self.searcher_c.call(req)])
+    }
+}
+
+struct SegmentSearchService {
+}
+
+impl SegmentSearchService {
+    fn new() -> SegmentSearchService {
+        SegmentSearchService {}
+    }
+}
+
+impl Service for SegmentSearchService {
     type Request = String;
     type Response = String;
     type Error = std::io::Error;
@@ -33,8 +69,30 @@ impl Service for SearchService {
     }
 }
 
-// New idea: build a fake version of Eriksen's psearch service: shows the main ideas
+struct RewriteRequest {
+    user: String,
+    query: String
+}
 
+struct RewriteService {}
+
+impl RewriteService {
+    fn new() -> RewriteService {
+        RewriteService {}
+    }
+}
+
+impl Service for RewriteService {
+    type Request = RewriteRequest;
+    type Response = String;
+    type Error = std::io::Error;
+    type Future = futures::Done<Self::Response, Self::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let new_query = if req.user.as_str() == "root" { "*".to_string() } else { req.query };
+        futures::done(Ok(new_query))
+    }
+}
 
 fn main() {
     // Core is the event loop. Its handle is a proxy for the service itself (so that we don't have
@@ -43,5 +101,5 @@ fn main() {
 
     let search = SearchService::new();
     let search_result = core.run(search.call("a".to_string())).unwrap();
-    println!("Result is: {}", search_result);
+    // println!("Result is: {}", search_result);
 }
